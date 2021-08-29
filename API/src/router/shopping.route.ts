a874e7728342d0c9f5db02107b33b4ba500f4d05
Dylan Vidal <thupapixd@gmail.com>
2021-08-28 21:43:22 -0500

    2

--- API/src/router/shopping.route.ts
+++ API/src/router/shopping.route.ts
@@ -10,7 +10,7 @@ export class ShoppingRoute{
     public routes(app:Application){
 
         //cart
-        app.get('/fegui_sajusa/api/v2/user/:id/car',[userJWT],  this.shoppingController.cart );
+        app.get('/fegui_sajusa/api/v2/user/:id/cart',[userJWT],  this.shoppingController.cart );
         app.post('/fegui_sajusa/api/v2/user/:ID/product/:id/add-cart',[userJWT],  this.shoppingController.addCart );
         app.patch('/fegui_sajusa/api/v2/user/:ID/update-cart/:id',[userJWT],  this.shoppingController.updateCart );
         app.delete('/fegui_sajusa/api/v2/user/:ID/delete-cart/:id',[userJWT],  this.shoppingController.deleteCart );
